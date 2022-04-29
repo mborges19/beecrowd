76d8411fa41bd391d6b458e7591079d6231a1448
mborges19 <theus.borges18@gmail.com>
2022-04-28 22:05:28 -0300

    Resolução em Python

--- bee_1067.py
+++ bee_1067.py
@@ -1,6 +1,7 @@
 #Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
 
 x = int(input())
-
+while 1 > x or x > 1000:
+    x = int(input())
 for c in range(1, x+1, 2):
     print(c)
