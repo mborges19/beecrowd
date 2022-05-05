ff39fa6cfb91414f5dd5537e382a9748543cc9f3
mborges19 <theus.borges18@gmail.com>
2022-05-05 19:05:30 -0300

    Resolução da AC1 em Python

--- bee_1125.py
+++ bee_1125.py
@@ -2,10 +2,10 @@ nota_trabalho = float(input())
 nota_prova = float(input())
 media = (nota_prova + nota_trabalho) / 2
 
-if media >= 6 and (nota_prova >= 6 or nota_prova >= 2):
+if media >= 6:
     print('aprovado')
 
-elif 6 > nota_trabalho >= 6 or nota_trabalho >= 2:
+elif nota_trabalho >= 6 or nota_trabalho >= 2:
     print('talvez com a sub')
 
 else:
